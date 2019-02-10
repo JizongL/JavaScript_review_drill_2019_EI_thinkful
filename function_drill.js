90a317a0112d45005f945075264f95120f591c94
tmmgeek <2015rpro@gmail.com>
2019-02-09 23:39:33 -0500

    forth drill

--- function_drill.js
+++ function_drill.js
@@ -1,7 +1,7 @@
-function createGreeting(){
-    return 'Hi, my name is LJZ';
+function createGreeting(name,age){
+    return `My name is ${name} and I am ${age} years old`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting("LJZ",100);
 console.log(greeting1);
 
