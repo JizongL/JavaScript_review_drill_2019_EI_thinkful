eb834d1fe5a6b46bfacb3dcdbef77bab0ef76411
tmmgeek <2015rpro@gmail.com>
2019-02-10 00:24:49 -0500

    added second function beyond and completed second drill

--- function_drill2.js
+++ function_drill2.js
@@ -2,3 +2,24 @@ function jediName(firstName, lastName){
     return lastName.slice(0,3)+firstName.slice(0,2);
 }
 console.log(jediName("John","Smith"));
+
+function beyond(num){
+    if(num ===-Infinity || num ===Infinity){
+        console.log("And beyond");
+    }
+    else if(num < Infinity && num > 0) {
+        console.log("To infinity");
+    }
+    else if(num > -Infinity && num < 0){
+        console.log("To negative infinity");
+    }
+    else if(num===0){
+        console.log("Staying home");
+    }
+};
+
+beyond(Infinity);
+beyond(-Infinity);
+beyond(10);
+beyond(-10);
+beyond(0);
