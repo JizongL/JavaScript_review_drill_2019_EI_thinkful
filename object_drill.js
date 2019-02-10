3221c872921a7044de90aa5a8d61a8334013ddc2
tmmgeek <2015rpro@gmail.com>
2019-02-10 18:29:50 -0500

    complete task 2 iterating over

--- object_drill.js
+++ object_drill.js
@@ -1,2 +1,30 @@
+// Object Drills
+// Object initializers and methods
+loaf = {
+    flour:300,
+    water:210,
+};
+// log out flour and water property
+console.log(loaf.flour,loaf.water)
+// add an empty method called hydration
+loaf['hydration']="";
 
-loaf = {};
+loaf.hydration= function(){
+        return this.water/100};
+
+console.log(loaf.hydration())        
+
+// Iterating over object property
+
+objectA = {
+    foo:12, 
+    bar:30, 
+    fum:21, 
+    quux:45,
+    spam:33
+}
+
+// objectA.object doesn't seem to work.
+for(object in objectA){
+    console.log(objectA[object])
+}
