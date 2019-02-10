d5e94194af36cc338749093d68aadd1ef8458cf1
tmmgeek <2015rpro@gmail.com>
2019-02-10 16:09:23 -0500

    added bonus=> fixed the word time and times for plural and singular issue

--- higher_order_function_drill.js
+++ higher_order_function_drill.js
@@ -62,3 +62,44 @@ console.log(filteredNames) // => ['Rich', 'Ray']
 
 // Bonus
 console.log(myNames.filter( name => name[0]==='R'));
+
+
+// function tells location of hazard and how many times // that hzard has issued an alert. 
+function hazardWarningCreator(typeOfWarning){
+	let warningCounter = 0;
+	// function as return value
+	return function(location){
+		// warningCounter is available here because 
+		// hazardWArningCreator() is its parent, so the // scope is inclusive. 
+
+		warningCounter+=1;
+		let times = "";
+		// bonus for time and times
+		if(warningCounter >=2 || warningCounter ===0)
+		times = 'time(s)'
+		else times = 'time';
+		// logging messages
+		console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+
+		console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} ${times} today!`);
+
+	};
+}
+
+// rocksWarning is pointed to the return of the 
+// rocksWarning(),and its return is a function. 
+// so, rockWarning is a secondary function
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+
+const iceWarning = hazardWarningCreator('Heavy ice is on the Road');
+
+const snowWarning = hazardWarningCreator('Snow storm is hitting');
+
+iceWarning('Jackson,MS')
+rocksWarning('Main St and Pacific Ave');
+rocksWarning('Main St and Pacific Ave');
+snowWarning('Center City, Pa')
+
+
+
+
