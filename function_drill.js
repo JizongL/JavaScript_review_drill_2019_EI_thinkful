6b4d308a0f0fd709cdbd9923d3fe05b4a36e1c2a
tmmgeek <2015rpro@gmail.com>
2019-02-09 23:45:49 -0500

    fifth drill

--- function_drill.js
+++ function_drill.js
@@ -1,5 +1,7 @@
 function createGreeting(name,age){
-    return `My name is ${name} and I am ${age} years old`;
+    const yearOfBrith = 2019-age;
+    return `My name is ${name} and I am ${age} years old\n 
+I was born in ${yearOfBrith}`;
 }
 
 const greeting1 = createGreeting("LJZ",100);
